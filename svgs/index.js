7a709b6f3218d252ace3b2df553f65bd519db3e6
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2020-06-30 06:33:19 +0300

    Added SVGs and Fonts

---
+++ svgs/index.js
@@ -0,0 +1,6 @@
+import { ReactComponent as HomeIcon } from '../svgs/home.svg';
+import { ReactComponent as SearchIcon } from '../svgs/search.svg';
+import { ReactComponent as LibraryIcon } from '../svgs/library.svg';
+import { ReactComponent as Logo } from '../svgs/logo.svg';
+
+export { HomeIcon, SearchIcon, LibraryIcon, Logo };
